3473648b9fa63005e9741441d1a41e1ec0bac2a5
Game94mod005 <aghkocholo91471@atomicmail.io>
2026-08-15 14:43:57 +0330

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,25 @@ ADMIN_ID = 7960216716
 # اطلاعات پرداخت
 CARD_NUMBER = "6219-8618-5590-7118"
 CARD_OWNER = "ابوالفضل خانی نژاد"
+
+
+
+# 🔥 AghaKocholo VPN Bot PRO
+
+BOT_TOKEN = "8923844495:AAGQ4z30Bopv_oThOsMqGvFn5evC9BB3ZEg"
+
+# آیدی عددی ادمین اصلی
+ADMIN_ID = 7960216716
+
+
+# 💳 اطلاعات پرداخت
+CARD_NUMBER = "6219-8618-5590-7118"
+CARD_OWNER = "ابوالفضل خانی نژاد"
+
+
+# 📞 پشتیبانی
+SUPPORT = "@Game94mod000"
+
+
+# 🏷️ نام ربات
+BOT_NAME = "⚡️🚀AghaKocholoVpns⚡️🚀"
