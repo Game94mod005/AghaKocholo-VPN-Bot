ee0c01c779dbb4ca601f6350930d3e58d65092b5
Game94mod005 <aghkocholo91471@atomicmail.io>
2026-08-15 14:20:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import asyncio
-import os
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
+from config import BOT_TOKEN, ADMIN_ID
 
-TOKEN = os.getenv("BOT_TOKEN")
-
-bot = Bot(TOKEN)
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
+
 @dp.message()
-async def start(message: Message):
-    await message.answer("🔥 AghaKocholo VPN Bot فعال شد ✅")
+async def messages(message: Message):
+
+    if message.text == "/start":
+        await message.answer(
+            "🔥 AghaKocholo VPN\n\n"
+            "خوش آمدید 👋\n\n"
+            "🛒 خرید VPN\n"
+            "📦 سرویس‌های من\n"
+            "💳 کیف پول\n"
+            "🆘 پشتیبانی"
+        )
+
+    elif message.text == "/admin":
+        if message.from_user.id == ADMIN_ID:
+            await message.answer(
+                "👑 پنل مدیریت\n\n"
+                "📊 آمار\n"
+                "📦 سفارش‌ها\n"
+                "👥 کاربران\n"
+                "⚙ تنظیمات"
+            )
+        else:
+            await message.answer("❌ دسترسی ندارید")
+
 
 async def main():
+    print("🔥 AghaKocholo VPN Bot Started")
     await dp.start_polling(bot)
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+    asyncio.run(main())
